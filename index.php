f93564e293e341d5765a354ffb508787ad53222e
Ole Magnus <32321558+lordolem@users.noreply.github.com>
2022-01-04 00:53:04 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" charset=utf-8>
+        <meta name="viewport" content="width=device-width, initial-scale=1" charset="utf-8">
         <link rel="stylesheet" href="style-index.css" type="text/css">
         <link rel="stylesheet" href="main.css" type="text/css">
         <title>Ole Magnus</title>
